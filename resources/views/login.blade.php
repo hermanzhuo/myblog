ffda3f7eff66c52c6d16838cde6a1381e8c1047a
hermanzhuo <zjw19950809@qq.com>
2021-10-02 15:45:01 +0800

    上传我的博客代码

---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登录</title>
+    <style>
+        .news{
+
+        }
+    </style>
+</head>
+<body>
+
+<a href="{{url("/")}}">首页</a>
+<a href="{{url("/login")}}">登录</a>
+<a href="{{url("/reg")}}">注册</a>
+<div class = "login_form">
+    <form >
+
+    </form>
+</div>
+
+这是登录页
+
+</body>
+</html>
---
+++ resources/views/reg.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>注册</title>
+</head>
+<body>
+
+<a href="{{url("/")}}">首页</a>
+<a href="{{url("/login")}}">登录</a>
+<a href="{{url("/reg")}}">注册</a>
+
+
+这是注册页
+</body>
+</html>
